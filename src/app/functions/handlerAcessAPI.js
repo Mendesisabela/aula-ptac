b7cd6d9e2d2741887463426ad3733733773a3019
Mendesisabela <isabelapicoli26@gmail.com>
2023-08-15 17:18:21 -0400

    FUNFANDO !!!!

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,15 +1,51 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const Usuarios = [
+    {
+        name : "mendes",
+        email : "isabelapicoli26@gmail.com",
+        password : "mene",
+        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+
+{
+    name : "vitoria",
+    email : "vitoria@gmail.com",
+    password : "toria",
+    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+
+{
+    name : "isabel",
+    email : "isabel@gmail.com",
+    password : "belinha",
+    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+}
+
+
+]
+
+
+const getUserAuthenticated = (userLogin) => {
+    let usermach = {};
+    
+    Usuarios.map((user) =>{
+        if(user.email === userLogin.email && user.password == userLogin.password){
+            usermach = user
+        }
+    })
+
+    return usermach
 
 }
 
 const getUsers = () =>{
-        [
-            name = "mendes",
-            email = "isabelapicoli26@gmail.com",
-            password = "mene",
-            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-        ]
+        return Usuarios
+
+
 }
+
+
+
+
 export { getUsers, getUserAuthenticated };
