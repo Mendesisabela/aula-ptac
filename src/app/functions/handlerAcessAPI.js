240b60e9a5ef6ff98d8f8b2e3e8edb2517373033
Mendesisabela <isabelapicoli26@gmail.com>
2023-08-15 16:39:32 -0400

    teste

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -5,6 +5,11 @@ const getUserAuthenticated = (user) => {
 }
 
 const getUsers = () =>{
-        
+        [
+            name = "mendes",
+            email = "isabelapicoli26@gmail.com",
+            password = "mene",
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ]
 }
 export { getUsers, getUserAuthenticated };
