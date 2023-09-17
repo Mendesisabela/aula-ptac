4b9863992fbd1980f9d6d1fa8f61d0332983a91c
Isabela Mendes <isabelapicoli26@gmail.com>
2023-09-17 16:23:15 -0300

    register css

---
+++ src/app/pages/register/page.js
@@ -0,0 +1,36 @@
+'use client'
+import { Suspense } from "react";
+import 'react-toastify/dist/ReactToastify.min.css';
+import { ToastContainer, toast } from 'react-toastify';
+import styles from './style.css'
+
+function Register() {
+    const notify =() => toast.success("Usuário cadastrado com sucesso!");
+ 
+    return (
+    <body>
+
+
+      <div class="page"> 
+    <form class="register">
+        <h1>Registrar</h1>
+        <p>Digite suas informações de registro nos campos abaixo.</p>
+
+        <label for="text">Nome</label>
+        <input type="text" placeholder="Digite seu nome"/>
+        <label for="text">E-mail</label>
+        <input type="text" placeholder="Digite seu e-mail"/>
+        <label for="password">Senha</label>
+        <input type="password" placeholder="Digite sua senha"/>
+
+        <button className="button" onClick={notify}>Register</button>
+        <ToastContainer />
+    </form>
+    </div>
+
+    </body>
+
+
+    );
+};
+export default Register
