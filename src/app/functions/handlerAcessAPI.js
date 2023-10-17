0d9ca89683a50a89029669e21b41794ae3f204c8
Mendesisabela <isabelapicoli26@gmail.com>
2023-10-17 17:30:18 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,67 +1,24 @@
 'use server'
 
-const Usuarios = [
-    {
-        name : "João Santos",
-        email : "joao.santos@gmail.com",
-        password : "123",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    
-    {
-        name : "Mendes",
-        email : "isabela@gmail.com",
-        password : "mene",
-        token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-},
+const url = "https://aula-17-10-roan.vercel.app/"
 
-{
-    name : "Gaby",
-    email : "gaby@gmail.com",
-    password : "maria",
-    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-},
-    
-{
-    name : "Vitória",
-    email : "vitoria@gmail.com",
-    password : "toria",
-    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-},
-
-{
-    name : "Isabel",
-    email : "isabel@gmail.com",
-    password : "belinha",
-    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-},
-
-{
-    name : "Karen",
-    email : "karen@gmail.com",
-    password : "kariii",
-    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-},
-]
-
-
-const getUserAuthenticated = (userLogin) => {
-    let usermach = {};
-    
-    Usuarios.map((user) =>{
-        if(user.email === userLogin.email && user.password == userLogin.password){
-            usermach = user
-        }
-    })
-
-    return usermach
-
+const getUserAuthenticated = async (user) => {
+   const responseOfApi = await fetch(url + "/user/authenticated",
+            {
+                method: "POST",
+                headers:{"Content-Type":"Application/json"},
+                body: JSON.stringify(user)
+            }
+   );
+   const userAuth = await responseOfApi.json();
+   return userAuth;
+       
 }
 
+
+
 const getUsers = () =>{
-        return Usuarios
-
-
+    
 }
 
 
